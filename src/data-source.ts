81974b6f1909b206f7c1a8e9b4905086d3d83038
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2025-01-20 00:32:39 +0500

    add user module

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,27 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+import * as dotenv from "dotenv";
+import { User } from "./entity/User.entity";
+
+dotenv.config();
+
+const { POSTGRES_HOST, DB_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE, NODE_ENV } =
+  process.env;
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: POSTGRES_HOST,
+  port: parseInt(DB_PORT || "5432"),
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
+  ssl:true,
+
+  synchronize: NODE_ENV === "dev" ? false : false,
+//logging logs sql command on the treminal
+  logging: NODE_ENV === "dev" ? false : false,
+  entities: [User],
+  migrations: [__dirname + "/migration/*.ts"],
+  subscribers: [],
+});
